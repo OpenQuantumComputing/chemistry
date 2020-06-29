500e408da51d469cf2b1f62694f40b627014d2c4
nielsaNTNU <nielsh.aase@hotmail.com>
2020-06-29 10:21:48 +0200

    Recreating the results from the notebook

--- quantum_chem_VQE_N2.py
+++ quantum_chem_VQE_N2.py
@@ -144,3 +144,28 @@ def plot_accuracy(ccsd_energies, VQE_energies, exact_energies, distances, distan
     ax2.legend()
     plt.tight_layout()
     plt.show()
+
+redo_caculations = False
+save_calcuations = False
+
+if redo_caculations:
+    distances_N2 = np.arange(1.0, 2.6, 0.1)
+    exact_energies_N2 = get_exact_energies_N2(distances_N2)
+    VQE_energies_N2 = get_VQE_energies_N2(distances_N2)
+    distances_ccsd_N2 = np.arange(1.0, 2.6, 0.2)
+    ccsd_energies_N2 = get_ccsd_energies_N2(distances_ccsd_N2, print_info=True)
+    if save_calcuations:
+        np.savetxt("data/distances_N2.csv", distances_N2, delimiter=",")
+        np.savetxt("data/exact_energies_N2.csv", exact_energies_N2, delimiter=",")
+        np.savetxt("data/VQE_energies_N2.csv", VQE_energies_N2, delimiter=",")
+        np.savetxt("data/distances_ccsd_N2.csv", distances_ccsd_N2, delimiter=",")
+        np.savetxt("data/ccsd_energies_N2.csv", ccsd_energies_N2, delimiter=",")
+else:
+    distances_N2 = np.genfromtxt("data/distances_N2.csv", delimiter=",")
+    exact_energies_N2 = np.genfromtxt("data/exact_energies_N2.csv", delimiter=",")
+    VQE_energies_N2 = np.genfromtxt("data/VQE_energies_N2.csv", delimiter=",")
+    distances_N2 = np.genfromtxt("data/distances_ccsd_N2.csv", delimiter=",")
+    ccsd_energies_N2 = np.genfromtxt("data/distances_ccsd_N2.csv", delimiter=",")
+
+print(distances_ccsd_N2)
+plot_accuracy(ccsd_energies_N2, VQE_energies_N2, exact_energies_N2, distances_N2, distances_ccsd_N2)
