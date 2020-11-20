ec4899c63e4a22f8e14521592f8c2b4eb0e90d0f
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2020-11-20 11:47:39 +0100

    update

--- H4.py
+++ H4.py
@@ -14,49 +14,89 @@ from qiskit.circuit.library import EfficientSU2
 from qiskit.aqua.components.optimizers import COBYLA, SPSA, SLSQP
 from qiskit.aqua.operators import Z2Symmetries
 from qiskit import IBMQ, BasicAer, Aer
-from qiskit.chemistry.drivers import PySCFDriver, UnitsType
+from qiskit.chemistry.drivers import PySCFDriver, UnitsType, Molecule
 from qiskit.chemistry import FermionicOperator
 from qiskit import IBMQ
 from qiskit.aqua import QuantumInstance
 from qiskit.ignis.mitigation.measurement import CompleteMeasFitter
 from qiskit.providers.aer.noise import NoiseModel
 
-from classical_ccsd_calculations import *
+from openfermion.hamiltonians import MolecularData
+from openfermionpsi4 import run_psi4
+
+from pyscf import gto, scf
 
 global g_energy, g_params
+
 def printvalues(count, paramset,means,estimator):
     global g_energy, g_params
     g_energy[count] = means
     g_params[count] = paramset
     #print(count, paramset, means, estimator )
 
-def get_qubit_op_HF(angle):
+def get_geometry(angle):
     R = 1.738
     angle *= np.pi/180 #conversion to radians
-    a = np.round(np.sin(angle / 2) * R, 3)
-    b = np.round(np.cos(angle / 2) * R, 3)
-    driver = PySCFDriver(atom="H " + str(a) + " " + str(b) + " .0; H " + str(-a) + " " + str(b) + " .0; H " +\
-                                      str(a) + " " + str(-b) + " .0; " + "H " + str(-a) + " " + str(-b) + " .0",
-                             unit=UnitsType.ANGSTROM, charge=0, spin=0, basis='sto3g')
-    molecule = driver.run()
-    return np.diag(molecule.one_body_integrals)
+    a = R * np.cos(angle/2)
+    b = R * np.sin(angle/2)
+    geometry = []
+    geometry.append(("H", [ a,  b, 0.]))
+    geometry.append(("H", [-a,  b, 0.]))
+    geometry.append(("H", [ a, -b, 0.]))
+    geometry.append(("H", [-a, -b, 0.]))
+    return geometry
+
+def get_molecule(angle):
+    multiplicity = 1
+    geometry = get_geometry(angle)
+    molecule = Molecule(geometry=geometry, multiplicity=multiplicity)
+    return molecule
+
+def get_onebodyintegrals_HF(angle):
+    molecule = get_molecule(angle)
+    driver = PySCFDriver(molecule=molecule)
+    qmolecule = driver.run()
+    return np.diag(qmolecule.one_body_integrals)
 
 def get_qubit_op(angle):
-    R = 1.738
-    angle *= np.pi/180 #conversion to radians
-    a = np.round(np.sin(angle / 2) * R, 3)
-    b = np.round(np.cos(angle / 2) * R, 3)
-    driver = PySCFDriver(atom="H " + str(a) + " " + str(b) + " .0; H " + str(-a) + " " + str(b) + " .0; H " +\
-                                      str(a) + " " + str(-b) + " .0; " + "H " + str(-a) + " " + str(-b) + " .0",
-                             unit=UnitsType.ANGSTROM, charge=0, spin=0, basis='sto3g')
-    molecule = driver.run()
-    num_particles = molecule.num_alpha + molecule.num_beta
-    num_spin_orbitals = molecule.num_orbitals * 2
-    ferOp = FermionicOperator(h1=molecule.one_body_integrals, h2=molecule.two_body_integrals)
+    molecule = get_molecule(angle)
+    driver = PySCFDriver(molecule=molecule)
+    qmolecule = driver.run()
+    num_particles = qmolecule.num_alpha + qmolecule.num_beta
+    num_spin_orbitals = qmolecule.num_orbitals * 2
+    ferOp = FermionicOperator(h1=qmolecule.one_body_integrals, h2=qmolecule.two_body_integrals)
     qubitOp = ferOp.mapping(map_type='parity', threshold=0.00000001)
     qubitOp = Z2Symmetries.two_qubit_reduction(qubitOp, num_particles)
-    shift = molecule.nuclear_repulsion_energy
+    shift = qmolecule.nuclear_repulsion_energy
     return qubitOp, num_particles, num_spin_orbitals, shift
+
+def get_ccsd_energy(angle):
+    basis = 'sto-3g'
+    multiplicity = 1
+
+    geometry = get_geometry(angle)
+    moleculedata = MolecularData(geometry, basis, multiplicity, description=str(round(angle, 2)))
+    molecule = run_psi4(moleculedata, run_scf=True, run_ccsd=True, delete_input=True, delete_output=True)
+    return molecule.ccsd_energy
+
+def get_quccsd_energy(angle, exact=False):
+    qubitOp, num_particles, num_spin_orbitals, shift = get_qubit_op(angle)
+
+    exact_energy=None
+    if exact:
+        result = NumPyEigensolver(qubitOp).run()
+        exact_energy = np.real(result.eigenvalues) + shift
+
+    initial_state = HartreeFock(num_spin_orbitals, num_particles, qubit_mapping='parity') 
+    var_form = UCCSD(num_orbitals=num_spin_orbitals, num_particles=num_particles, initial_state=initial_state, qubit_mapping='parity')
+
+    global g_energy, g_params
+    g_energy = {}
+    g_params = {}
+
+    vqe = VQE(qubitOp, var_form, optimizer, callback=printvalues)
+    quccsd_energy = np.real(vqe.run(backend)['eigenvalue'] + shift)
+    return quccsd_energy, exact_energy
 
 
 backend = BasicAer.get_backend("statevector_simulator")
@@ -66,66 +106,41 @@ res=10
 angles = np.linspace(85, 90, res)
 angles_sym = np.linspace(85,95, 2*res-1)
 exact_energies = []
-quccsdvqe_energies = []
+ccsd_energies = []
+quccsd_energies = []
 optimizer = SLSQP(maxiter=5)
 #optimizer = COBYLA(maxiter=250)
 
-diagvalues = np.zeros((len(angles), 8))
-
+### 1) Hartree Fock-energies
 i = 0
+diagvalues = np.zeros((len(angles), 8))
 for angle in angles:
-    diagvalues[i,:] = get_qubit_op_HF(angle)
+    diagvalues[i,:] = get_onebodyintegrals_HF(angle)
     i += 1
-
-print(diagvalues)
-
-plt.plot(angles, diagvalues)
+plt.plot(angles, diagvalues, 'x-')
 plt.savefig('HF.png')
 
+### 2) energies obtianed by exact, ccsd, and quccsd methods
 for angle in angles:
-    qubitOp, num_particles, num_spin_orbitals, shift = get_qubit_op(angle)
-    result = NumPyEigensolver(qubitOp).run()
-    exact_energies.append(np.real(result.eigenvalues) + shift)
-    initial_state = HartreeFock(
-        num_spin_orbitals,
-        num_particles,
-        qubit_mapping='parity'
-    ) 
-    var_form = UCCSD(
-        num_orbitals=num_spin_orbitals,
-        num_particles=num_particles,
-        initial_state=initial_state,
-        qubit_mapping='parity'
-    )
-
-    g_energy = {}
-    g_params = {}
-
-    vqe = VQE(qubitOp, var_form, optimizer, callback=printvalues)
-    vqe_result = np.real(vqe.run(backend)['eigenvalue'] + shift)
-    quccsdvqe_energies.append(vqe_result)
-    print("Angle=", np.round(angle, 2), "VQE Result:", vqe_result, "Exact Energy:", exact_energies[-1])
-    ind = min(g_energy, key=g_energy.get)
-    print(g_energy[ind]+shift, g_params[ind])
-    print(list(g_energy.items())[-1]+shift, list(g_params.items())[-1])
-    print()
-    print()
-    print()
+    print("quccsd and exact energy at", angle, "degrees")
+    quccsd_energy, exact_energy = get_quccsd_energy(angle, True)
+    print("ccsd at", angle, "degrees")
+    ccsd_energy = get_ccsd_energy(angle)
+    exact_energies.append(exact_energy)
+    ccsd_energies.append(ccsd_energy)
+    quccsd_energies.append(quccsd_energy)
     
-print("All energies have been calculated")
-
-ccsd_energies = list(get_ccsd_energies_H4(angles))
-
+### 3) use symmetry around 90 degrees
 for i in range(res-1):
     exact_energies.append(exact_energies[res-i-2])
-    quccsdvqe_energies.append(quccsdvqe_energies[res-i-2])
+    quccsd_energies.append(quccsd_energies[res-i-2])
     ccsd_energies.append(ccsd_energies[res-i-2])
 
 plt.clf()
-plt.plot(angles_sym, exact_energies, 'x-', label="Exact Energy")
-plt.plot(angles_sym, quccsdvqe_energies, 'x-',label="VQE Energy")
-plt.plot(angles_sym, ccsd_energies, 'x-',label="CCSD Energy")
-plt.xlabel('Atomic distance (Angstrom)')
+plt.plot(angles_sym, exact_energies, 'x-', label="eigensolver")
+plt.plot(angles_sym, quccsd_energies, 'x-',label="q-UCCSD")
+plt.plot(angles_sym, ccsd_energies, 'x-',label="CCSD")
+plt.xlabel('Angle')
 plt.ylabel('Energy')
 plt.legend()
 plt.savefig('H4.png')
